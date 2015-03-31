1af9040278c14db703006c0fb83e3d0975b99660
wesleyharbeek <wesleyharbeek@outlook.com>
2015-03-31 10:15:13 +0200

    GameQuery background
    background werkt

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
-var background1 = new $.gameQuery.Animation({imageURL: "img/background1.png"});
-        var PLAYGROUND_WIDTH	= 700;
-        var PLAYGROUND_HEIGHT	= 250;
+$(function(){
+    var background1 = new $.gameQuery.Animation({imageURL: "img/background1.png"});
+        var PLAYGROUND_WIDTH = 700;
+        var PLAYGROUND_HEIGHT = 250;
+        var REFRESH_RATE = 30;
+
+        var smallStarSpeed        = 1; //pixels per frame
+        var mediumStarSpeed       = 3; //pixels per frame
+        var bigStarSpeed          = 5; //pixels per frame
 
         var background2 = new $.gameQuery.Animation({imageURL: "img/background2.png"});
         var background3 = new $.gameQuery.Animation({imageURL: "img/background3.png"});
@@ -8,7 +14,12 @@ var background1 = new $.gameQuery.Animation({imageURL: "img/background1.png"});
         var background5 = new $.gameQuery.Animation({imageURL: "img/background5.png"});
         var background6 = new $.gameQuery.Animation({imageURL: "img/background6.png"});
 
-        $("#background").
+        $("#playground").playground({
+            height: PLAYGROUND_HEIGHT, 
+            width: PLAYGROUND_WIDTH, 
+        keyTracker: true});
+
+        $("#background").playground({height: PLAYGROUND_HEIGHT, width: PLAYGROUND_WIDTH})
            .addSprite("background1", {animation: background1,
                        width: PLAYGROUND_WIDTH, height: PLAYGROUND_HEIGHT})
            .addSprite("background2", {animation: background2,
@@ -25,30 +36,37 @@ var background1 = new $.gameQuery.Animation({imageURL: "img/background1.png"});
                       width: PLAYGROUND_WIDTH, height: PLAYGROUND_HEIGHT,
                       posx: PLAYGROUND_WIDTH});
 
+    // $.setLoadBar("loadingBar", 400);
+    
+    $("#startbutton").click(function(){
+        $.playground().startGame(function(){
+            $("#welcomeScreen").remove();
+        });
+    });
 
-$.playground().registerCallback(function(){
-           //Offset all the pane:
-           var newPos = (parseInt($("#background1").css("left")) - smallStarSpeed - PLAYGROUND_WIDTH)
-                                  % (-2 * PLAYGROUND_WIDTH) + PLAYGROUND_WIDTH;
-          $("#background1").css("left", newPos);
+    $.playground().registerCallback(function() {
+       var newPos = (parseInt($("#background1").css("left")) - smallStarSpeed - PLAYGROUND_WIDTH)
+                              % (-2 * PLAYGROUND_WIDTH) + PLAYGROUND_WIDTH;
+      $("#background1").css("left", newPos);
 
-          newPos = (parseInt($("#background2").css("left")) - smallStarSpeed - PLAYGROUND_WIDTH)
-                             % (-2 * PLAYGROUND_WIDTH) + PLAYGROUND_WIDTH;
-          $("#background2").css("left", newPos);
+      newPos = (parseInt($("#background2").css("left")) - smallStarSpeed - PLAYGROUND_WIDTH)
+                         % (-2 * PLAYGROUND_WIDTH) + PLAYGROUND_WIDTH;
+      $("#background2").css("left", newPos);
 
-          newPos = (parseInt($("#background3").css("left")) - mediumStarSpeed - PLAYGROUND_WIDTH)
-                             % (-2 * PLAYGROUND_WIDTH) + PLAYGROUND_WIDTH;
-          $("#background3").css("left", newPos);
+      newPos = (parseInt($("#background3").css("left")) - mediumStarSpeed - PLAYGROUND_WIDTH)
+                         % (-2 * PLAYGROUND_WIDTH) + PLAYGROUND_WIDTH;
+      $("#background3").css("left", newPos);
 
-          newPos = (parseInt($("#background4").css("left")) - mediumStarSpeed - PLAYGROUND_WIDTH)
-                             % (-2 * PLAYGROUND_WIDTH) + PLAYGROUND_WIDTH;
-          $("#background4").css("left", newPos);
+      newPos = (parseInt($("#background4").css("left")) - mediumStarSpeed - PLAYGROUND_WIDTH)
+                         % (-2 * PLAYGROUND_WIDTH) + PLAYGROUND_WIDTH;
+      $("#background4").css("left", newPos);
 
-          newPos = (parseInt($("#background5").css("left")) - bigStarSpeed - PLAYGROUND_WIDTH)
-                             % (-2 * PLAYGROUND_WIDTH) + PLAYGROUND_WIDTH;
-          $("#background5").css("left", newPos);
+      newPos = (parseInt($("#background5").css("left")) - bigStarSpeed - PLAYGROUND_WIDTH)
+                         % (-2 * PLAYGROUND_WIDTH) + PLAYGROUND_WIDTH;
+      $("#background5").css("left", newPos);
 
-          newPos = (parseInt($("#background6").css("left")) - bigStarSpeed - PLAYGROUND_WIDTH)
-                             % (-2 * PLAYGROUND_WIDTH) + PLAYGROUND_WIDTH;
-          $("#background6").css("left", newPos);
-        }, REFRESH_RATE);
+      newPos = (parseInt($("#background6").css("left")) - bigStarSpeed - PLAYGROUND_WIDTH)
+                         % (-2 * PLAYGROUND_WIDTH) + PLAYGROUND_WIDTH;
+      $("#background6").css("left", newPos);
+    }, REFRESH_RATE);
+});
