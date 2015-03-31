352e05a0215add0d48a1ef24606df95cecd3617e
wesleyharbeek <wesleyharbeek@outlook.com>
2015-03-31 11:22:45 +0200

    GameQuery bevat nu planky
    yay

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
+function Player(node) {
+    this.node = node;
+    
+    this.grace = false;
+    this.replay = 3;
+    this.respawnTime = -1;
+    
+    return true;
+};
+
 $(function(){
     var background1 = new $.gameQuery.Animation({imageURL: "img/background1.png"});
-        var PLAYGROUND_WIDTH = 700;
-        var PLAYGROUND_HEIGHT = 250;
+        var PLAYGROUND_WIDTH = 1000;
+        var PLAYGROUND_HEIGHT = 400;
         var REFRESH_RATE = 30;
+        var playerAnimation = new Array();
 
         var smallStarSpeed        = 1; //pixels per frame
         var mediumStarSpeed       = 3; //pixels per frame
@@ -36,7 +47,7 @@ $(function(){
                       width: PLAYGROUND_WIDTH, height: PLAYGROUND_HEIGHT,
                       posx: PLAYGROUND_WIDTH});
 
-    // $.setLoadBar("loadingBar", 400);
+    // $().setLoadBar("loadingBar", 400);
     
     $("#startbutton").click(function(){
         $.playground().startGame(function(){
@@ -45,6 +56,28 @@ $(function(){
     });
 
     $.playground().registerCallback(function() {
+        $("#player")[0].player.update();
+              if (jQuery.gameQuery.keyTracker[65]){ // Left (a)
+                var nextpos = parseInt($("#player").css("left"))-5;
+                if(nextpos > 0){
+                  $("#player").css("left", ""+nextpos+"px");
+                }
+              }
+        
+              if (jQuery.gameQuery.keyTracker[68]){ // Right (d)
+                var nextpos = parseInt($("#player").css("left"))+5;
+                if(nextpos < PLAYGROUND_WIDTH - 100){
+                  $("#player").css("left", ""+nextpos+"px");
+                }
+              }
+        
+              if (jQuery.gameQuery.keyTracker[87]){ // up (w)
+                var nextpos = parseInt($("#player").css("top"))-3;
+                if(nextpos > 0){
+                  $("#player").css("top", ""+nextpos+"px");
+                }
+              }
+        
        var newPos = (parseInt($("#background1").css("left")) - smallStarSpeed - PLAYGROUND_WIDTH)
                               % (-2 * PLAYGROUND_WIDTH) + PLAYGROUND_WIDTH;
       $("#background1").css("left", newPos);
@@ -56,17 +89,12 @@ $(function(){
       newPos = (parseInt($("#background3").css("left")) - mediumStarSpeed - PLAYGROUND_WIDTH)
                          % (-2 * PLAYGROUND_WIDTH) + PLAYGROUND_WIDTH;
       $("#background3").css("left", newPos);
-
-      newPos = (parseInt($("#background4").css("left")) - mediumStarSpeed - PLAYGROUND_WIDTH)
-                         % (-2 * PLAYGROUND_WIDTH) + PLAYGROUND_WIDTH;
-      $("#background4").css("left", newPos);
-
-      newPos = (parseInt($("#background5").css("left")) - bigStarSpeed - PLAYGROUND_WIDTH)
-                         % (-2 * PLAYGROUND_WIDTH) + PLAYGROUND_WIDTH;
-      $("#background5").css("left", newPos);
-
-      newPos = (parseInt($("#background6").css("left")) - bigStarSpeed - PLAYGROUND_WIDTH)
-                         % (-2 * PLAYGROUND_WIDTH) + PLAYGROUND_WIDTH;
-      $("#background6").css("left", newPos);
     }, REFRESH_RATE);
+    
+    playerAnimation["idle"] = new $.gameQuery.Animation({imageURL: "img/main.png"});
+    
+    $.playground().addGroup("actors", {width: PLAYGROUND_WIDTH, height: PLAYGROUND_HEIGHT})
+        .addGroup("player", {posx: 200, posy: 320,
+             width: 50, height: 50})
+        .addSprite("playerBody", {animation: playerAnimation["idle"], posx: 0, posy: 0, width: 50, height: 50});
 });
