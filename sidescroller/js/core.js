64f5b9bc2b64826672112030ac78f1726c3f3e58
wesleyharbeek <wesleyharbeek@outlook.com>
2015-04-14 10:03:57 +0200

    Commit

--- sidescroller/js/core.js
+++ sidescroller/js/core.js
@@ -7,6 +7,7 @@ var canvas = document.getElementById("canvas"),
     ctx = canvas.getContext("2d"),
     width = 800,
     height = 400,
+    
     player = {
       x : width/2,
       y : height - 5,
@@ -17,6 +18,15 @@ var canvas = document.getElementById("canvas"),
       velY: 0,
       jumping: false
     },
+    
+    object = {
+        x: width / 2,
+        y: height - 122,
+        
+        width: 30,
+        height: 60
+    },
+        
     keys = [],
     friction = 0.8,
     gravity = 0.2;
@@ -26,21 +36,27 @@ canvas.height = height;
  
 function update(){
   // check keys
+    
     if (keys[38] || keys[32]) {
         // up arrow or space
+        
       if(!player.jumping){
        player.jumping = true;
        player.velY = -player.speed*2;
       }
     }
+    
     if (keys[39]) {
         // right arrow
+        
         if (player.velX < player.speed) {             
             player.velX++;         
          }     
-    }     
+    }   
+    
     if (keys[37]) {         
-        // left arrow         
+        // left arrow   
+        
         if (player.velX > -player.speed) {
             player.velX--;
         }
@@ -53,13 +69,16 @@ function update(){
     player.x += player.velX;
     player.y += player.velY;
  
-    if (player.x >= width-player.width) {
+    if (player.x >= width-player.width)
+    {
         player.x = width-player.width;
-    } else if (player.x <= 0) {         
+    }
+    else if (player.x <= 0)
+    {         
         player.x = 0;     
-    }    
+    }
   
-    if(player.y >= height-player.height - 55){
+    if (player.y >= height-player.height - 55){
         player.y = height - player.height - 55;
         player.jumping = false;
     }
@@ -67,6 +86,7 @@ function update(){
     ctx.clearRect(0,0,width,height);
     
     ctx.drawImage(document.getElementById("mainChar"), player.x, player.y);
+    ctx.drawImage(document.getElementById("objectHill"), object.x, object.y); 
     
     // ctx.fillStyle = "red";
     // ctx.fillRect(player.x, player.y, player.width, player.height);
